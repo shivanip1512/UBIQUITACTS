8978d895ccbee207c0a78e0a337481699ef50d58
shivanip1512 <shivanipacharne10@gmail.com>
2022-02-01 22:20:35 +0530

    setting up view(templates)

--- src/main/java/com/shivani/management/smart/controller/HomeController.java
+++ src/main/java/com/shivani/management/smart/controller/HomeController.java
@@ -1,26 +1,21 @@
 package com.shivani.management.smart.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.shivani.management.smart.dao.UserRepository;
-import com.shivani.management.smart.entity.User;
 
 @Controller
 public class HomeController {
 
-	@Autowired
-	private UserRepository repository;
+	@GetMapping("/")
+	public String homeHandler(Model model) {
+		model.addAttribute("pageTitle","Home - Ubiquitacts");
+		return "home";
+	}
 	
-	@GetMapping("/test")
-	@ResponseBody
-	public String test() {
-		User user = new User();
-		user.setName("Shivani");
-		user.setPassword("123");
-		repository.save(user);
-		return "working";
+	@GetMapping("/about")
+	public String aboutHandler(Model model) {
+		model.addAttribute("pageTitle","About Us");
+		return "about";
 	}
 }
